16cb65710c6ed7d3e8a2711786a01867ea8a01a1
snoopy811005 <snoopy811005@gmail.com>
2022-02-04 23:09:05 +0800

    add file dialog & cut out img_controller.py

--- controller.py
+++ controller.py
@@ -1,10 +1,10 @@
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import QImage, QPixmap
-import cv2
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
 from UI import Ui_MainWindow
+from img_controller import img_controller
 
-class MainWindow_controller(QtWidgets.QMainWindow):
+class MainWindow_controller(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
@@ -13,55 +13,21 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
     def setup_control(self):
         # Step 1 -> read and display image
-        self.img_path = 'dog.jpg' # image file path
-        self.img = None # image with OpenCV type(BGR)
-        self.qimg = None # image with QImage type(RGB)
-        self.qpixmap = None # image with QPixmap type
-        self.qpixmap_height = None # get QPixmap's height for resize
-        self.__display_img()
+        self.img_path = 'image/empty.jpg'  # image file path
+        self.img_controller = img_controller(img_path=self.img_path, ui=self.ui)
 
         # Step 2 -> set connection(when button clicked)
-        self.ui.btn_zoom_in.clicked.connect(self.set_zoom_in)
-        self.ui.btn_zoom_out.clicked.connect(self.set_zoom_out)
+        self.ui.btn_zoom_in.clicked.connect(self.img_controller.set_zoom_in)
+        self.ui.btn_zoom_out.clicked.connect(self.img_controller.set_zoom_out)
+        self.ui.btn_open_image.clicked.connect(self.open_file)
 
         # Step 3 -> set scrollArea property
         self.ui.scrollArea.setWidgetResizable(True) # activate scroll bar
-        self.ui.label.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop) # align image at left/top
+        self.ui.label.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop) # align image at scrollArea's left_top
 
-    def set_zoom_in(self):
-        # minimum value = original image * 10%
-        self.qpixmap_height = max(int(self.qpixmap.height() * 0.1), self.qpixmap_height - 100)
-        self.__resize_display_img()
-
-    def set_zoom_out(self):
-        # maximum value = original image * 200%
-        self.qpixmap_height = min(self.qpixmap_height + 100, int(self.qpixmap.height() * 2))
-        self.__resize_display_img()
-
-    def __display_img(self):
-        # OpenCV type(BGR)
-        self.img = cv2.imread(self.img_path)
-        height, width, channel = self.img.shape
-        print(f"height={height}, width={width}, channel={channel}")
-        # QImage type(RGB)
-        bytesPerline = 3 * width
-        self.qimg = QImage(self.img, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
-        # QPixmap type
-        self.qpixmap = QPixmap.fromImage(self.qimg)
-        self.qpixmap_height = self.qpixmap.height()
-        # display image
-        self.ui.label.setPixmap(QPixmap.fromImage(self.qimg))
-        # display text(image shape)
-        self.__display_text_imgShape((self.qpixmap.width(), self.qpixmap.height()))
-
-    def __resize_display_img(self):
-        # resize image
-        scaled_qpixmap = self.qpixmap.scaledToHeight(self.qpixmap_height)
-        print(f"image shape = ({scaled_qpixmap.width()}, {scaled_qpixmap.height()})")
-        # display image
-        self.ui.label.setPixmap(scaled_qpixmap)
-        # display text(image shape)
-        self.__display_text_imgShape((scaled_qpixmap.width(), scaled_qpixmap.height()))
-
-    def __display_text_imgShape(self, imgShape):
-        self.ui.label_img_shape.setText(f"image shape = ({imgShape[0]}, {imgShape[1]})")
+    def open_file(self):
+        img_format = ('bmp', 'jpeg', 'jpg', 'png', 'tiff', 'tif', 'pic')
+        filename, filetype = QFileDialog.getOpenFileName(self, "Open file", "./") # open current path dialog
+        # read and display image
+        self.img_path = 'image/sad.jpg' if filename.split('.')[-1] not in img_format else filename
+        self.img_controller.set_img_path(self.img_path)
---
+++ img_controller.py
@@ -0,0 +1,59 @@
+from PyQt5.QtGui import QImage, QPixmap
+import cv2
+
+class img_controller(object):
+    def __init__(self, img_path, ui):
+        self.img_path = img_path # image file path
+        self.ui = ui # ui object
+
+        self.img = None  # image with OpenCV type(BGR)
+        self.qimg = None  # image with QImage type(RGB)
+        self.qpixmap = None  # image with QPixmap type
+        self.qpixmap_height = None  # get QPixmap's height for resize
+
+        self.__read_img()
+        self.__display_img()
+
+    def set_zoom_in(self):
+        # minimum value = original image * 2%
+        self.qpixmap_height = max(int(self.qpixmap.height() * 0.02), self.qpixmap_height - 100)
+        self.__display_img()
+
+    def set_zoom_out(self):
+        # maximum value = original image * 200%
+        self.qpixmap_height = min(self.qpixmap_height + 100, int(self.qpixmap.height() * 2))
+        self.__display_img()
+
+    def set_img_path(self, img_path):
+        self.img_path = img_path
+        self.__read_img()
+        self.__display_img()
+
+    def __read_img(self):
+        # OpenCV type(BGR)
+        try:
+            self.img = cv2.imread(self.img_path)
+        except:
+            self.img = cv2.imread('image/sad.jpg')
+        height, width, channel = self.img.shape
+        print(f"height={height}, width={width}, channel={channel}")
+        # QImage type(RGB)
+        bytesPerline = 3 * width
+        self.qimg = QImage(self.img, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
+        # QPixmap type
+        self.qpixmap = QPixmap.fromImage(self.qimg)
+        self.qpixmap_height = self.qpixmap.height()
+
+    def __display_img(self):
+        # resize image
+        scaled_qpixmap = self.qpixmap.scaledToHeight(self.qpixmap_height)
+        print(f"current img shape = ({scaled_qpixmap.width()}, {scaled_qpixmap.height()})")
+        # display image
+        self.ui.label.setPixmap(scaled_qpixmap)
+        # display text(image shape)
+        self.__display_text_imgShape((scaled_qpixmap.width(), scaled_qpixmap.height()))
+
+    def __display_text_imgShape(self, scaledImgShape):
+        self.ui.label_img_shape_original.setText(f"Original img shape = ({self.qpixmap.width()}, {self.qpixmap.height()})")
+        self.ui.label_img_shape_current.setText(f"Current img shape = ({scaledImgShape[0]}, {scaledImgShape[1]})")
+
